adcbbf47923d6358641bdeb63f521801f3b02ec9
Ryan Dixon <ryan555@sky.com>
2022-07-26 12:04:50 +0100

    init

---
+++ components/Sidebar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+    BellIcon,
+    HashtagIcon,
+    BookmarkIcon,
+    CollectionIcon,
+    DotsCircleHorizontalIcon,
+    MailIcon,
+    UserIcon,
+    HomeIcon
+} from '@heroicons/react/outline'
+
+function Sidebar() {
+  return <div>
+    <img className='h-10 w-10' src="https://links.papareact.com/drq" alt="" />
+  </div>
+  
+}
+
+export default Sidebar
